03a6690978bfde79766c2b87b626e7b3a5ddec9a
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2025-05-02 07:56:11 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://goto.zkit.above.gay/',
+    bare:'focus-bare2.onrender.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
